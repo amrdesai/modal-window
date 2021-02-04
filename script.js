0e0b8bad297f1e0ab0c0281bde5113685ebb954f
Aamer Desai <56724460+minm333@users.noreply.github.com>
2021-02-04 14:58:35 -0500

    Add modal open/close functionality

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+// Variables
+const modalEl = document.querySelector('.modal');
+const bgOverlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+const showModalBtns = document.querySelectorAll('.show-modal');
+
+// Function: Toggle Modal
+const toggleModal = () => {
+    modalEl.classList.toggle('hidden');
+    bgOverlay.classList.toggle('hidden');
+};
+
+// Event Listener: Show Modal event listner
+showModalBtns.forEach((currentItem) => {
+    currentItem.addEventListener('click', toggleModal);
+});
+
+// Event Listener: Close modal btn
+closeModalBtn.addEventListener('click', toggleModal);
+
+// Event Listener: Close modal when clicked on overlay
+bgOverlay.addEventListener('click', toggleModal);
+
+// Event Listener: Close modal if 'ESC' key press
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        toggleModal();
+    }
+});
