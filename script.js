38e8ef824c7d143514eb84a273e46972b545243b
Aamer Desai <56724460+minm333@users.noreply.github.com>
2021-02-04 15:05:20 -0500

    Made seperate functions for open/close modal to fix bug

--- script.js
+++ script.js
@@ -7,25 +7,29 @@ const closeModalBtn = document.querySelector('.close-modal');
 const showModalBtns = document.querySelectorAll('.show-modal');
 
 // Function: Toggle Modal
-const toggleModal = () => {
-    modalEl.classList.toggle('hidden');
-    bgOverlay.classList.toggle('hidden');
+const closeModal = () => {
+    modalEl.classList.add('hidden');
+    bgOverlay.classList.add('hidden');
+};
+
+// Function: Close Modal
+const openModal = () => {
+    modalEl.classList.remove('hidden');
+    bgOverlay.classList.remove('hidden');
 };
 
 // Event Listener: Show Modal event
 showModalBtns.forEach((currentItem) => {
-    currentItem.addEventListener('click', toggleModal);
+    currentItem.addEventListener('click', openModal);
 });
 
 // Event Listener: Close modal btn event
-closeModalBtn.addEventListener('click', toggleModal);
+closeModalBtn.addEventListener('click', closeModal);
 
 // Event Listener: Close modal when clicked on overlay
-bgOverlay.addEventListener('click', toggleModal);
+bgOverlay.addEventListener('click', closeModal);
 
 // Event Listener: Close modal on 'ESC' key press
 document.addEventListener('keyup', (event) => {
-    if (event.key === 'Escape') {
-        toggleModal();
-    }
+    if (event.key === 'Escape') closeModal();
 });
